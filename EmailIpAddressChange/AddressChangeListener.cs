6514a9f854b00fc4ff0fd47de41afa90d7be4b29
ainzhebeikin <ainzhebeikin@gmail.com>
2014-05-05 10:22:45 +0400

    Monitor change of specified interface

---
+++ EmailIpAddressChange/AddressChangeListener.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using NLog;
+
+namespace EmailIpAddressChange
+{
+    internal class AddressChangeListener : IDisposable
+    {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private readonly string _interfaceName;
+
+        private IPAddress _lastAddress;
+
+        public AddressChangeListener(string interfaceName)
+        {
+            _interfaceName = interfaceName;
+            _lastAddress = IPAddress.None;
+            LogNetworkInterfaces();
+            NetworkChange.NetworkAddressChanged += NetworkChangeOnNetworkAddressChanged;
+        }
+
+        public void Dispose()
+        {
+            NetworkChange.NetworkAddressChanged -= NetworkChangeOnNetworkAddressChanged;
+        }
+
+        private void NetworkChangeOnNetworkAddressChanged(object sender, EventArgs eventArgs)
+        {
+            LogNetworkInterfaces();
+        }
+
+        private void LogNetworkInterfaces()
+        {
+            foreach (var ipAddressInformation in NetworkInterface.GetAllNetworkInterfaces()
+                .Where(@interface => string.Equals(@interface.Name, _interfaceName))
+                .SelectMany(@interface => @interface.GetIPProperties().UnicastAddresses)
+                .Where(ipAddressInformation => !ipAddressInformation.Address.Equals(_lastAddress)))
+            {
+                _logger.Info(ipAddressInformation.Address);
+                _lastAddress = ipAddressInformation.Address;
+                break;
+            }
+        }
+    }
+}
--- EmailIpAddressChange/Service.cs
+++ EmailIpAddressChange/Service.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Net;
 using System.Net.NetworkInformation;
 using System.ServiceProcess;
 using NLog;
@@ -11,19 +14,31 @@ namespace EmailIpAddressChange
 
         private readonly Switch _switch = new Switch();
 
+        private readonly string _interfaceName;
+
+        private AddressChangeListener _addressChangeListener;
+
         public Service()
         {
             InitializeComponent();
             CanStop = true;
+            _interfaceName = ConfigurationManager.AppSettings["InterfaceName"];
         }
 
         protected override void OnStart(string[] args)
         {
             if (_switch.TurnOn())
             {
-                _logger.Info("Started");
-                LogNetworkInterfaces();
-                NetworkChange.NetworkAddressChanged += NetworkChangeOnNetworkAddressChanged;
+                if (null == _interfaceName)
+                {
+                    _logger.Error("InterfaceName is not specified; aborting");
+                    Stop();
+                }
+                else
+                {
+                    _addressChangeListener = new AddressChangeListener(_interfaceName);
+                    _logger.Info("Started");
+                }
             }
         }
 
@@ -31,32 +46,12 @@ namespace EmailIpAddressChange
         {
             if (_switch.TurnOff())
             {
-                NetworkChange.NetworkAddressChanged -= NetworkChangeOnNetworkAddressChanged;
-                _logger.Info("Stopped");
-            }
-        }
-
-        private void NetworkChangeOnNetworkAddressChanged(object sender, EventArgs eventArgs)
-        {
-            LogNetworkInterfaces();
-        }
-
-        private void LogNetworkInterfaces()
-        {
-            foreach (var @interface in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                _logger.Info("Id = {0}, Name = {1}, Type = {2}, Status = {3}, Speed = {4}",
-                             @interface.Id,
-                             @interface.Name,
-                             @interface.NetworkInterfaceType,
-                             @interface.OperationalStatus,
-                             @interface.Speed);
-                foreach (var address in @interface.GetIPProperties().UnicastAddresses)
+                if (null != _addressChangeListener)
                 {
-                    _logger.Info("Unicast address family = {0}, address = {1}",
-                                 address.Address.AddressFamily,
-                                 address.Address);
+                    _addressChangeListener.Dispose();
+                    _addressChangeListener = null;
                 }
+                _logger.Info("Stopped");
             }
         }
     }
